877b3964b17d28b8d1804a10130136058f9b558b
ws23 <linyutsu23@gmail.com>
2015-03-28 00:19:30 +0800

    update reg page

--- index.php
+++ index.php
@@ -1,10 +1,32 @@
-<form action="reg.php" method="post">
-	<label>報名人數</label><br />
-	<select name="sel">
-	<?php
-		for($i=1;$i<=50;$i++)
-			echo "<option value\"{$i}\">{$i} 人</option>"; 
-	?>
-	</select>
-	<input type="submit" value="submit" />
-</form>
+<!Doctype html>
+<html>
+<head>
+	<meta charset="utf8" />
+	<link rel="stylesheet" href="3.3.4/css/bootstrap.min.css" />
+	<script src="3.3.4/js/bootstrap.min.hs"></script>
+	<title>愛奇藝股份有限公司 - Seminar Register</title>
+</head>	
+
+<body>
+
+<div class="jumbotron">
+	<div class="container">
+		<h1>Seminar Information</h1>
+		<p>This is a template for a simple marketing or informational website. It includes a large callout called a jumbotron and three supporting pieces of content. Use it as a starting point to create something more unique.</p>
+		<p><a class="btn btn-primary btn-lg" href="#" role="button">Learn more &raquo;</a></p>
+		
+
+
+		<form action="reg.php" method="post">
+			<h3>報名人數</h3>
+			<select name="sel">
+			<?php
+				for($i=1;$i<=50;$i++)
+					echo "<option value\"{$i}\">{$i} 人</option>"; 
+			?>
+			</select>
+			<input class="btn btn-success" type="submit" value="submit" />
+		</form>
+	</div>
+</div>
+</body>
--- reg.php
+++ reg.php
@@ -1,20 +1,70 @@
+<!Doctype html>
+<html>
+<head>
+	<meta charset="utf8" />
+	<link rel="stylesheet" href="3.3.4/css/bootstrap.min.css" />
+	<script src="3.3.4/js/bootstrap.min.js"></script>
+</head>
+
+<body role="document">
+	
+
+<div class="jumbotron">
+	<div class="container">
+
+	
 <?php
 	$len = $_POST['sel']; 
 
 ?>
+
+<div class="page-header">
+	<h1>活動名稱 報名系統</h2>
+</div>
+
 <form action="#" method="post">
+
+<div class="row">
+<table class="table">
+<thead>
+<tr>
+	<th class="col-md-1">#</th>
+	<th class="col-md-1">姓名</th>
+	<th class="col-md-1">聯絡電話</th>
+	<th class="col-md-2">Email</th>
+	<th class="col-md-2">所屬單位</th>
+	<th class="col-md-1">餐飲習慣</th>
+</tr>
+
 <?php
 	for($i=1; $i<=$len; $i++){
-	?>
-		<p>
-		<label>報名者<?php echo $i; ?>Name: </label>
-			<input type="text"><br />
-		<label>報名者<?php echo $i; ?>Other: </label>
-			<input type="text"></p>
-<?php
+?>	
+<tr>
+	<td><?php echo $i; ?></td>
+	<td><div class="form-group"><input type="text" name="name<?php echo $i; ?>" placeholder="Name" class="form-control" /></div></td>
+	<td><div class="form-group"><input type="text" name="phone<?php echo $i; ?>" placeholder="Phone" class="form-control" /></div></td>
+	<td><div class="form-group"><input type="text" name="email<?php echo $i; ?>" placeholder="Email" class="form-control" /></div></td>
+	<td><?php if ($i==1) echo '<div class="form-group"><input type="text" name="company" placeholder="Company" class="form-control" /></div>'; ?></td>
+	<td>
+		<select name="eat<?php echo $i; ?>">
+			<option value="all">葷</option>
+			<option value="vege">素</option>
+		</select>
+	</td>
+</tr>
+<?php		
 	}
-?>
 
-	<input type="submit" value="submit">
+?>	
+	
+</table>
+</div>
+	
+<input class="btn btn-lg btn-success" type="submit" value="submit">
 </form>
+
+	</div>
+</div>
+
+</body>
 
